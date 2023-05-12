39d2f9d7f6cf06a349acce235b58d27058d5c7d7
wherethacoffee <whr.thacoff23@gmail.com>
2023-05-11 21:54:46 -0600

    Se agregó el archivo principal del proyecto

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');	
+const myConnection = require('express-myconnection');
+
+const app = express();
+
+//importando rutas
+//const projectRoutes = require('./routes');
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'admin'}, 'single')
+);
+
+app.use(express.urlencoded({extended: false}));
+
+//rutas
+//app.use('/', projectRoutes);
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log('listening on port 3000');
+});
