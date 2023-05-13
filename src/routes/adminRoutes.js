6e8f0f8a7f1e95797953b12bf6cf3c78aa6d42b5
wherethacoffee <whr.thacoff23@gmail.com>
2023-05-13 17:12:09 -0600

    Se ha creado la funcionalidad del crud de admin

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 
-router.get('/', adminController.inicio);
+router.get('/', adminController.listar);
+router.post('/add', adminController.agregar);
+router.get('/delete/:id', adminController.eliminar);
+router.get('update/:id', adminController.editar);
+router.post('update/:id', adminController.modificar);
 
 module.exports = router;
