9b990c65d728983d994949547222f8750a26c904
WFJACC <wfjacc.blz@gmail.com>
2023-05-13 02:22:23 -0600

    Se agrego el controlador listar, en municipioController

--- src/controllers/municipioController.js
+++ src/controllers/municipioController.js
@@ -1,9 +1,21 @@
 const controller = {}
 
 controller.inicio = (req, res) => {
-    res.render('GestorMunicipio')
+    res.render('GestorAdmin')
 }
 
+controller.listar = (req, res) =>{
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM municipio', (err, municipio) => {
+        if(err){
+            res.json(err);
+        }
+        res.render('GestorMunicipio', {
+            data: municipio
+        })
+        })
+    })
+}
 
 
 
