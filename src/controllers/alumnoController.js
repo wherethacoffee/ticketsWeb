4a56a21e72eea1a65e9271a7c18296887d94cc1d
WFJACC <wfjacc.blz@gmail.com>
2023-05-15 01:12:57 -0600

    Modificacion de sentencia sql para el formulario de alumno

--- src/controllers/alumnoController.js
+++ src/controllers/alumnoController.js
@@ -16,18 +16,48 @@ controller.inicio = (req, res) => {
 controller.agregar = (req, res) => {
     const data = req.body;
     
+    const values = [
+        data.curp,
+        data.nombre,
+        data.paterno,
+        data.materno,
+        data.telefono,
+        data.correo,
+        data.nivel,
+        data.asunto,
+        data.idmunicipio,
+        data.idmunicipio
+        ]
+
     // Validar la CURP antes de insertarla en la base de datos
     if (!validarCURP(data.curp)) {
-        res.redirect('alumno/inicio')
+        res.status(400).send('La CURP no es válida');
+        return;
     }
 
     req.getConnection((err, conn) => {
-        conn.query('INSERT INTO alumno set ?', [data], (err, alumno) => {
-            if (err) {
-                res.json(err);
-            } else {
-                res.redirect('alumno/inicio')}
-        });
+        const sql = `
+    INSERT INTO alumno (curp, nombre, paterno, materno, telefono, correo, nivel, asunto, turno, idmunicipio)
+    SELECT 
+    ? as curp, 
+    ? as nombre, 
+    ? as paterno, 
+    ? as materno, 
+    ? as telefono, 
+    ? as correo,
+    ? as nivel, 
+    ? as asunto, 
+    (SELECT IFNULL(MAX(turno), 0) + 1 FROM alumno WHERE idmunicipio = ?) as turno,
+    ? as idmunicipio;
+`;
+
+    conn.query(sql, values, (err, result) => {
+    if (err) {
+        res.json(err);
+    } else {
+        res.redirect('/alumno/inicio')
+    }
+});
     })
 }
 
