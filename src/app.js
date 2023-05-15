fe49350166cefa31b924a02f1dde3acf9ae438d9
WFJACC <wfjacc.blz@gmail.com>
2023-05-15 02:00:18 -0600

    Se agrego una ruta

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(morgan('dev'));
 app.use(myConnection(mysql, {
     host: 'localhost',
     user: 'root',
-    password: 'admin',
+    password: '',
     port: 3306,
     database: 'ticketsdb'}, 'single')
 );
